7d03ccd0a3b11212ac073292fae50c5ac65a5fae
devleandrodias <leandrodbdias@gmail.com>
2020-07-16 20:56:30 -0300

    adicionando botoes ao App

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -3,14 +3,34 @@ import 'package:flutter/material.dart';
 main(List<String> args) => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  final List<String> perguntas = [
+    'Qual é a sua cor favorita?',
+    'Qual é o seu animal favorito'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('Perguntas e Respostas'),
-      ),
-      body: Text('Olá Flutter!!'),
-    ));
+            appBar: AppBar(
+              title: Text('Perguntas e Respostas'),
+            ),
+            body: Column(
+              children: <Widget>[
+                Text(perguntas[1]),
+                RaisedButton(
+                  child: Text('Resposta 1'),
+                  onPressed: null,
+                ),
+                RaisedButton(
+                  child: Text('Resposta 2'),
+                  onPressed: null,
+                ),
+                RaisedButton(
+                  child: Text('Resposta 3'),
+                  onPressed: null,
+                ),
+              ],
+            )));
   }
 }
