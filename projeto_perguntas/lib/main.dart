0531cf995c1b14f9ff773051da3f556b08da76f4
devleandrodias <leandrodbdias@gmail.com>
2020-07-16 21:07:13 -0300

    trabalhando como funcoes ao clicar em um botao

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -3,13 +3,22 @@ import 'package:flutter/material.dart';
 main(List<String> args) => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
-  final List<String> perguntas = [
-    'Qual é a sua cor favorita?',
-    'Qual é o seu animal favorito'
-  ];
+  void responder() {
+    print('Pergunta respondida');
+  }
+
+  void Function() funcaoQueRetornaOutraFuncao() {
+    return () {
+      print('Pergunta respondida #03');
+    };
+  }
 
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito'
+    ];
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
@@ -20,15 +29,18 @@ class PerguntaApp extends StatelessWidget {
                 Text(perguntas[1]),
                 RaisedButton(
                   child: Text('Resposta 1'),
-                  onPressed: null,
+                  onPressed: () => print(
+                      'Resposta #01'), // passando a função como parâmetro e não invocando a função
                 ),
                 RaisedButton(
                   child: Text('Resposta 2'),
-                  onPressed: null,
+                  onPressed: () {
+                    print('Resposta #02');
+                  },
                 ),
                 RaisedButton(
                   child: Text('Resposta 3'),
-                  onPressed: null,
+                  onPressed: funcaoQueRetornaOutraFuncao(),
                 ),
               ],
             )));
