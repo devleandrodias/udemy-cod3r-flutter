5d8894cf3edbb4a2f171c701b8dbbfb24af52b4b
devleandrodias <leandrodbdias@gmail.com>
2020-07-16 22:42:06 -0300

    rendenizando lista de forma imperativo

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -30,6 +30,12 @@ class _PerguntaAppState extends State<PerguntaApp> {
       }
     ];
 
+    List<Widget> respostas = [];
+
+    for (String textResp in perguntas[_perguntaSelecionada]['respostas']) {
+      respostas.add(Resposta(textResp, _responder));
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -38,9 +44,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: <Widget>[
             Questao(perguntas[_perguntaSelecionada]['texto']),
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),
+            ...respostas,
           ],
         ),
       ),
