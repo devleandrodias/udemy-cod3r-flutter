a468736357822619f2d1c9d21376db7b4bc46f88
devleandrodias <leandrodbdias@gmail.com>
2020-07-19 10:51:44 -0300

    estrutura inicial do app de despesas pessoais

---
+++ expenses/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+main(List<String> args) => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+        backgroundColor: Colors.purple[700],
+      ),
+      body: Center(
+        child: Text('Versão inicial app.'),
+      ),
+    );
+  }
+}
