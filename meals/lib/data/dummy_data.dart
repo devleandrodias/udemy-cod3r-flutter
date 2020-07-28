34a8084c4a2d1aa3d6906c1c190f31e3842f116d
devleandrodias <leandrodbdias@gmail.com>
2020-07-28 19:19:34 -0300

    criando projeto novo, e comecando a entender o GridView

---
+++ meals/lib/data/dummy_data.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: '1',
+    title: '1',
+    color: Colors.orange,
+  ),
+];
