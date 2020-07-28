9c7442a0787d43cfcf9457dc18580be02bfe314a
devleandrodias <leandrodbdias@gmail.com>
2020-07-28 20:17:15 -0300

    trabalhando com navagacao entre telas no flutter

--- meals/lib/widgets/category_item.widget.dart
+++ meals/lib/widgets/category_item.widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
+import 'package:meals/screens/categories_meals.screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
@@ -8,23 +9,38 @@ class CategoryItem extends StatelessWidget {
     @required this.category,
   });
 
+  void _selectedCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoriesMealsScree();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.headline6,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      borderRadius: BorderRadius.circular(13),
+      splashColor: Theme.of(context).primaryColor,
+      onTap: () => _selectedCategory(context),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(13),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
       ),
     );
