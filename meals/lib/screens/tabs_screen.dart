ef49dc32f5a29f358c08ce52946cadf633895dcd
devleandrodias <leandrodbdias@gmail.com>
2020-08-01 13:33:07 -0300

    criando rodapé de navegacao

--- meals/lib/screens/tabs_screen.dart
+++ meals/lib/screens/tabs_screen.dart
@@ -2,34 +2,79 @@ import 'package:flutter/material.dart';
 import 'package:meals/screens/categories.screen.dart';
 import 'package:meals/screens/favorites_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+
+  final List<Map<String, Object>> _screens = [
+    {'title': 'Lista de Categorias', 'screen': CategoriesScreen()},
+    {'title': 'Lista de Favoritos', 'screen': FavoriteScreen()}
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Vamos Cozinhar?'),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: Icon(Icons.favorite_border),
-                text: 'Favoritos',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(title: Text(_screens[_selectedScreenIndex]['title'])),
+      body: _screens[_selectedScreenIndex]['screen'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectScreen,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedScreenIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text('Categorias'),
           ),
-        ),
-        body: TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoriteScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            title: Text('Categorias'),
+          ),
+        ],
       ),
     );
   }
 }
+// class TabsScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 2,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Vamos Cozinhar?'),
+//           bottom: TabBar(
+//             tabs: [
+//               Tab(
+//                 icon: Icon(Icons.category),
+//                 text: 'Categorias',
+//               ),
+//               Tab(
+//                 icon: Icon(Icons.favorite_border),
+//                 text: 'Favoritos',
+//               ),
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: [
+//             CategoriesScreen(),
+//             FavoriteScreen(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
