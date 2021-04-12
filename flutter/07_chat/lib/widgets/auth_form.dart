06c46ff623d2a980656e3b9b4a2b36bb2ffbde2f
devleandrodias <leandrodbdias@gmail.com>
2021-04-12 11:19:27 -0300

    criando base da tela de login

---
+++ flutter/07_chat/lib/widgets/auth_form.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Nome',
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'E-mail',
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Entrar'),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text('Criar uma nova conta'),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
