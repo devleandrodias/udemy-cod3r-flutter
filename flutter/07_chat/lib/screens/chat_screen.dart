4620a522e58b8f888cf5ce6ab5471fabe2a6c81b
devleandrodias <leandrodbdias@gmail.com>
2021-04-12 11:08:40 -0300

    trabalhando com streambuilder, acesso ao firestore, configurando ios

--- flutter/07_chat/lib/screens/chat_screen.dart
+++ flutter/07_chat/lib/screens/chat_screen.dart
@@ -1,19 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) {
-          return Container(
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: Text('Funcionou'),
-            ),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('chat').snapshots(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final documents = snapshot.data.documents;
+
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) {
+              return Container(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    documents[index]['text'],
+                  ),
+                ),
+              );
+            },
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
