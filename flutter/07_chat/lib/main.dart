583c5d3bdaa261d6e661c020af615da36afd5006
devleandrodias <leandrodbdias@gmail.com>
2021-04-12 15:27:11 -0300

    logando e deslogando do app com firebase

--- flutter/07_chat/lib/main.dart
+++ flutter/07_chat/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:chat/screens/auth_screen.dart';
+import 'package:chat/screens/chat_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +15,7 @@ class FlutterChat extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.indigo,
-        primaryColorBrightness: Brightness.light,
+        primaryColorBrightness: Brightness.dark,
         backgroundColor: Colors.indigo,
         accentColor: Colors.pink,
         accentColorBrightness: Brightness.dark,
@@ -26,7 +28,16 @@ class FlutterChat extends StatelessWidget {
         ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (ctx, userSnapshot) {
+          if (userSnapshot.hasData) {
+            return ChatScreen();
+          } else {
+            return AuthScreen();
+          }
+        },
+      ),
     );
   }
 }
